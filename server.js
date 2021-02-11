fe602751ea2ccbb27ef170c1af54fe6fb02a26e9
Rome Bell <romebell5@gmail.com>
2021-02-11 11:40:26 -0800

    models: Build out model, add validations, add functions

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
-const app = express();
 
+const app = express();
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+// Controllers
+app.use('/auth', require('./controllers/auth'));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -17,9 +19,6 @@ app.get('/', (req, res) => {
 app.get('/profile', (req, res) => {
   res.render('profile');
 });
-
-app.use('/auth', require('./routes/auth'));
-
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
