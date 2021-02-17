745243d9152a0d6983f0d35fe59f1d47e7403037
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-02-16 22:40:56 -0800

    worked on updating auth for purposes of project 2

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+// loaded auth from server.js through app.use
 
+//render page auth/signup from views when /auth/signup url is accessed
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
@@ -9,4 +11,13 @@ router.get('/login', (req, res) => {
   res.render('auth/login');
 });
 
+router.post('/signup', (req, res) => {
+  // what does this need to do before it renders the page?
+  // needs to log user in
+  res.render('profile');
+})
+
+router.post('/login', (req, res) => {
+  res.render('profile')
+})
 module.exports = router;
