c3a1674dc3faa4307bd00b0fd2141341b3eb6596
jessica-au <AU.JESSICA.M@GMAIL.COM>
2021-02-18 14:39:50 -0800

    changed route for api results to the categories controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const unirest = require('unirest');
 
 // import database
 const db = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('categories')
-  });
+// router.get('/', (req, res) => {
+//    // res.render('categories')
+// });
 
-
+router.get('/', async (req, res) => {
+    try {
+        var listenNotesUrl = 'https://listen-api.listennotes.com/api/v2';
+        let response = await unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars')
+            .header('X-ListenAPI-Key', 'c61dffbeb6c54d508b1f8b24caa1c986')
+        response = await response.toJSON();
+        let podcastResults = response.body.results;
+        console.log(podcastResults[0])
+        res.render('categories', { podcastResults })
+    } catch (e) {
+        console.log(e)
+    }
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.use((req, res, next) => {
 // Controllers
 app.use('/auth', require('./controllers/auth'));
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('/profile', isLoggedIn, (req, res) => {
   const { id, name, email } = req.user.get();
@@ -58,18 +58,18 @@ app.get('/profile', isLoggedIn, (req, res) => {
 
 
 // GET / - main index of site
-app.get('/', async (req, res) => {
-  try {
-    var listenNotesUrl = 'https://listen-api.listennotes.com/api/v2';
-    let response = await unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars')
-      .header('X-ListenAPI-Key', 'c61dffbeb6c54d508b1f8b24caa1c986')
-    response = await response.toJSON();
-    let podcastResults = response.body.results;
-    console.log(podcastResults[0])
-    res.render('index', {podcastResults})
-  } catch (e) {
-    console.log(e)
-  }
+// app.get('/', async (req, res) => {
+//   try {
+//     var listenNotesUrl = 'https://listen-api.listennotes.com/api/v2';
+//     let response = await unirest.get('https://listen-api.listennotes.com/api/v2/search?q=star%20wars')
+//       .header('X-ListenAPI-Key', 'c61dffbeb6c54d508b1f8b24caa1c986')
+//     response = await response.toJSON();
+//     let podcastResults = response.body.results;
+//     console.log(podcastResults[0])
+//     res.render('categories', {podcastResults})
+//   } catch (e) {
+//     console.log(e)
+//   }
   //res.send(response);
 
   // Use request to call the API
@@ -82,7 +82,7 @@ app.get('/', async (req, res) => {
   //   var podcast = apiResponse.data.results;
   //   res.render('index', { });
   // })
-});
+// });
 
 // Imports all routes from the auth routes file
 app.use('/auth', require('./controllers/auth'));
